2c7b70853b238abed8d1f2509536b7a07a0f1e8e
goyang1103 <00bsj@naver.com>
2025-03-22 23:48:32 +0900

    연결끊기

--- src/App.js
+++ src/App.js
@@ -10,23 +10,7 @@ import PageShop from "./pages/PageShop";
 import PageHelp from "./pages/PageHelp";
 
 const App = () => {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route element={<CommonForm />}>
-            <Route path="/" element={<CleanJeong />} />
-            <Route path="/intro" element={<Introduce />} />
-            <Route path="/houseclean" element={<Pageone />} />
-            <Route path="/completion" element={<PageCompletion />} />
-            <Route path="/special" element={<PageSpecial />} />
-            <Route path="/shop" element={<PageShop />} />
-            <Route path="/help" element={<PageHelp />} />
-          </Route>
-        </Routes>
-      </Router>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
